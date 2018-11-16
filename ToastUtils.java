e74465eb77bb728f66bc9d06e641b8dbf73bb124
BluceZheng <35486046+BluceZheng@users.noreply.github.com>
2018-11-16 14:17:00 +0800

    Initial commit

---
+++ ToastUtils.java
@@ -0,0 +1,16 @@
+package com.zjh.opensourcelibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtils {
+    /**
+     * 显示吐司
+     *
+     * @param context
+     * @param str
+     */
+    public static void showToast(Context context, String str) {
+        Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+    }
+}
